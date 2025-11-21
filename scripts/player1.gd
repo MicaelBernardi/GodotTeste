3520f928f48f9ac6cd21c0b53e5a3781723d9e8d
Douglas Silva <131932027+namirso@users.noreply.github.com>
2025-11-21 18:10:09 -0300

    GAMEJAM

--- scripts/player1.gd
+++ scripts/player1.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 # --- MOVIMENTO ---
 const SPEED = 100.0
 const RUN_SPEED = 200.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -300.0
 const GRAVITY = 900.0
 
 # --- WALL MECHANICS ---
--- scripts/player2.gd
+++ scripts/player2.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -250.0
 const RUN_SPEED = 200.0
 
 @onready var animation := $animation as AnimatedSprite2D
